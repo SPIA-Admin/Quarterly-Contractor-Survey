f99e11c1ed8deadfbb607a4e9528c0ea356d2335
NateDogDotNet <NateDogDotNet@users.noreply.github.com>
2024-01-18 23:05:30 -0600

    created initial sample plots. added a pallet for the color blind. included themes that are publication-ready

--- Surveys/2024/Q1 2024 Report.R
+++ Surveys/2024/Q1 2024 Report.R
@@ -1,6 +1,9 @@
 library(readxl)
-library(dplyr)
+#library(dplyr)
 library(ggplot2)
+#library(viridisLite)
+library(viridis)
+library(ggthemes)
 
 # Replace with the path to your Excel file
 file_path <- ".\\Surveys\\2024\\Q1 2024 Contractor Survey (Responses).xlsx"
@@ -9,13 +12,13 @@ file_path <- ".\\Surveys\\2024\\Q1 2024 Contractor Survey (Responses).xlsx"
 survey_data <- read_excel(file_path)
 
 # View the first few rows of the data
-head(survey_data)
+#head(survey_data)
 
 # Load survey_categories
 source(".\\Surveys\\2024\\Q1 2024 SectionsAndColumns.R")
 source(".\\Surveys\\2024\\Q1 2024 SectionDemographics.R")
 
-plots <- create_plots()
+#plots <- create_plots()
 
 # Now you can display or use the plots
 #print(plots$Plot1)
@@ -30,30 +33,58 @@ demographic_plots <- create_demographic_visualizations(survey_data)
 
 # Print plots
 # Page 1: Provider Plot
-plot.new()
-title("Demographics Analysis - Service Providers")
+#plot.new()
+#title("Demographics Analysis - Service Providers")
 print(demographic_plots$ProviderPlot)
-mtext("This bar chart shows the distribution of service providers contracted with by the survey respondents.", side = 1, line = 4, adj = 0)
+#mtext("This bar chart shows the distribution of service providers contracted with by the survey respondents.", side = 1, line = 4, adj = 0)
 
 # Page 2: Services Plot
-plot.new()
-title("Demographics Analysis - Services Contracted For")
+#plot.new()
+#title("Demographics Analysis - Services Contracted For")
 print(demographic_plots$ServicesPlot)
-mtext("This bar chart represents the types of services that respondents are contracted for.", side = 1, line = 4, adj = 0)
+#mtext("This bar chart represents the types of services that respondents are contracted for.", side = 1, line = 4, adj = 0)
 
 # Page 3: Territory Plot
-plot.new()
-title("Demographics Analysis - Primary Territories of Routes")
+#plot.new()
+#title("Demographics Analysis - Primary Territories of Routes")
 print(demographic_plots$TerritoryPlot)
-mtext("The chart illustrates the primary territories of the respondents' routes, such as urban, suburban, and rural areas.", side = 1, line = 4, adj = 0)
+#mtext("The chart illustrates the primary territories of the respondents' routes, such as urban, suburban, and rural areas.", side = 1, line = 4, adj = 0)
 
 # Page 4: Delivery Type Plot
-plot.new()
-title("Demographics Analysis - Delivery Type Proportions")
+#plot.new()
+#title("Demographics Analysis - Delivery Type Proportions")
 print(demographic_plots$DeliveryTypePlot)
-mtext("This pie chart compares the percentage of deliveries made to residential addresses versus business addresses.", side = 1, line = 4, adj = 0)
+#mtext("This pie chart compares the percentage of deliveries made to residential addresses versus business addresses.", side = 1, line = 4, adj = 0)
 
 
 
 # Close the PDF device
 dev.off()
+
+
+
+
+
+# library(mapproj)
+# 
+# data(unemp, package = "viridis")
+# 
+# county_df <- map_data("county", projection = "albers", parameters = c(39, 45))
+# names(county_df) <- c("long", "lat", "group", "order", "state_name", "county")
+# county_df$state <- state.abb[match(county_df$state_name, tolower(state.name))]
+# county_df$state_name <- NULL
+# 
+# state_df <- map_data("state", projection = "albers", parameters = c(39, 45))
+# 
+# choropleth <- merge(county_df, unemp, by = c("state", "county"))
+# choropleth <- choropleth[order(choropleth$order), ]
+# 
+# ggplot(choropleth, aes(long, lat, group = group)) +
+#   geom_polygon(aes(fill = rate), colour = alpha("white", 1 / 2), linewidth = 0.2) +
+#   geom_polygon(data = state_df, colour = "white", fill = NA) +
+#   coord_fixed() +
+#   theme_minimal() +
+#   ggtitle("US unemployment rate by county") +
+#   theme(axis.line = element_blank(), axis.text = element_blank(),
+#         axis.ticks = element_blank(), axis.title = element_blank()) +
+#   scale_fill_viridis(option="turbo")
--- Surveys/2024/Q1 2024 SectionDemographics.R
+++ Surveys/2024/Q1 2024 SectionDemographics.R
@@ -16,35 +16,54 @@
 # 
 # return(list(Plot1 = plot1, Plot2 = plot2))
 # }
-
+viridis_option <- "D"
+SPIA_theme <- theme_economist()
 
 # Function to create demographic visualizations
 create_demographic_visualizations <- function(data) {
   # Bar Chart for Provider
-  provider_plot <- ggplot(data, aes(x = .data[[survey_categories$Demographics$Provider]])) +
+  provider_plot <- ggplot(data) +
+    aes(x = .data[[survey_categories$Demographics$Provider]],fill = .data[[survey_categories$Demographics$Provider]]) +
     geom_bar() +
-    theme_minimal() +
-    labs(title = "Distribution of Service Providers", x = "Service Provider", y = "Count")
+    scale_fill_viridis(option = viridis_option, direction = 1, discrete = TRUE) +
+    SPIA_theme +
+    theme(legend.position = "bottom")+
+    labs(title = "Distribution of Service Providers", x = "Service Provider", y = "Count") 
   
   # Bar Chart for Services Contracted For
-  services_plot <- ggplot(data, aes(x = .data[[survey_categories$Demographics$Services]])) +
+  services_plot <- ggplot(data)+
+    aes(x = .data[[survey_categories$Demographics$Services]],fill =.data[[survey_categories$Demographics$Services]]) +
     geom_bar() +
-    theme_minimal() +
+    scale_fill_viridis(option = viridis_option, direction = 1, discrete = TRUE) +
+    SPIA_theme +
+    theme(legend.position = "bottom")+
     labs(title = "Services Contracted For", x = "Services", y = "Count")
   
   # Bar Chart for Primary Territories
-  territory_plot <- ggplot(data, aes(x = .data[[survey_categories$Demographics$Territory]])) +
+  territory_plot <- ggplot(data)+
+    aes(x = .data[[survey_categories$Demographics$Territory]], fill = .data[[survey_categories$Demographics$Territory]]) +
     geom_bar() +
-    theme_minimal() +
+    scale_fill_viridis(option = viridis_option, direction = 1, discrete = TRUE) +
+    SPIA_theme +
+    theme(legend.position = "bottom")+
     labs(title = "Primary Territories of Routes", x = "Territories", y = "Count")
   
   # Pie Chart for Delivery Type (Residential vs Business)
   delivery_type_counts <- table(data[[survey_categories$Demographics$DeliveryType]])
-  delivery_type_plot <- ggplot(as.data.frame(delivery_type_counts), aes(x = "", y = Freq, fill = Var1)) +
+  # delivery_type_plot <- ggplot(as.data.frame(delivery_type_counts), aes(x = "", y = Freq, fill = Var1)) +
+  #   geom_bar(width = 1, stat = "identity") +
+  #   coord_polar("y", start = 0) +
+  #   scale_fill_viridis(discrete = TRUE, option=viridis_option) +
+  #   labs(title = "Delivery Type Proportions", fill = "Delivery Type")
+  delivery_type_plot <- ggplot(as.data.frame(delivery_type_counts))+
+    aes(x = "", y = Freq, fill = Var1) +
     geom_bar(width = 1, stat = "identity") +
     coord_polar("y", start = 0) +
-    theme_void() +
+    scale_fill_viridis(option = viridis_option, direction = 1, discrete = TRUE) +
+    SPIA_theme +
+    theme(legend.position = "bottom")+
     labs(title = "Delivery Type Proportions", fill = "Delivery Type")
+
   
   return(list(ProviderPlot = provider_plot, ServicesPlot = services_plot, TerritoryPlot = territory_plot, DeliveryTypePlot = delivery_type_plot))
 }

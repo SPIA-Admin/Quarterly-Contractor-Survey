61d9a8957c00679f591e0ba4c6989e5c41b6d4dc
NateDogDotNet <NateDogDotNet@users.noreply.github.com>
2024-05-29 09:34:07 -0500

    organized

---
+++ Surveys/2024/Q1/loadToDuckDb.R
@@ -0,0 +1,24 @@
+library(duckdb)
+library(ggplot2)
+library(dplyr)
+library(stringr)
+
+# Load Helper functions
+source(".\\Surveys\\2024\\DuckDbHelper.R")
+
+# Initialize DuckDB connection
+con <- dbConnect(duckdb::duckdb(), dbdir = ".\\Data\\duckdb_database.duckdb", read_only = FALSE)
+
+# List all Parquet files in the directory
+parquet_files <- list.files(".\\Data\\", pattern = "\\.parquet$", full.names = TRUE)
+
+# Phase 1: Conditionally load or append each Parquet file into DuckDB as a table
+for (file_path in parquet_files) {
+  table_name <- create_table_name(file_path)
+  load_or_append_from_parquet(con, table_name, file_path)
+}
+
+# Ensure all actions are completed and cleanup
+dbDisconnect(con) # Close the connection to commit all changes
+rm(con) # Remove the connection object
+gc() # Force garbage collection to free up resources
